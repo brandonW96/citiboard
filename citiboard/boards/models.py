72ed170783276958f88927ec470cd38fd0f0602e
Nathanael Kim <nathanael@nyu.edu>
2017-12-14 16:14:41 -0500

    nate test

--- citiboard/boards/models.py
+++ citiboard/boards/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import uuid
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
@@ -11,6 +12,7 @@ class Board(models.Model)
 	"""
 	#fields 
 	Board_ID = models.UUIDField(primary_key=True, default = uuid.uuid4, editable=False)
+	user = models.OneToOneField(User, blank = False, null = False)
 	Is_In_Use = False;
 	Is_Reserved = False; 
 	Is_Broken = False; 
@@ -21,4 +23,7 @@ class Board(models.Model)
 			return "Board %d is currently available" % (self.Board_ID)
 		else :
 			return "Board %d is currently unavailable" % (self.Board_ID)
+	
+class Rider(models.Model)
+	
 	
