0d9d9e656e565a8d62716594d4d734f4de5eaf97
Nathanael Kim <nathanael@nyu.edu>
2017-12-13 19:01:04 -0500

    Adding Boards Model
    Boards

--- citiboard/boards/models.py
+++ citiboard/boards/models.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
+import uuid
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class Board(models.Model)
+	""" 
+		Defining the model for Boards
+	"""
+	#fields 
+	Board_ID = models.UUIDField(primary_key=True, default = uuid.uuid4, editable=False)
+	Is_In_Use = False;
+	Is_Reserved = False; 
+	Is_Broken = False; 
+	AVAILABILITY = True; 
+	
+	def __str__ (self):
+		if AVAILABILITY  == True:
+			return "Board %d is currently available" % (self.Board_ID)
+		else :
+			return "Board %d is currently unavailable" % (self.Board_ID)
+	
