6f5a3944da02685d1ec4d3effcb7e6df181ff189
Brandon <bw1126@nyu.edu>
2017-12-14 20:51:08 -0500

    adding the rent, return, register, load balance callback files

---
+++ citiboard-node/routes/rent.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const route = express.Router();
+const mongoose = require('mongoose');
+require('../db');
+
+router.get('/rent', (req, res) => {
+	
+});
+
+module.exports = router;
